f3538b8f96db069b3e44c4d7aa2ca7bcea22dfe5
Nicolás Trujillo <tnicolas@uninorte.edu.co>
2021-10-06 10:55:20 -0500

    useCounter.test.js: finalizando pruebas en el archivo

--- src/tests/hooks/useCounter.test.js
+++ src/tests/hooks/useCounter.test.js
@@ -1,7 +1,80 @@
-
+import { renderHook, act } from '@testing-library/react-hooks';
+import { useCounter } from '../../hooks/useCounter';
 
 describe('Pruebasn en useCounter', () => {
     
-    
-    
+   test('debe retornar valores por defecto', () => {
+        
+    const { result } = renderHook( () => useCounter() );
+
+    expect( result.current.counter ).toBe( 10 );
+    expect( typeof result.current.increment ).toBe( 'function' );
+    expect( typeof result.current.decrement ).toBe( 'function' );
+
+
+   });
+   
+
+   test('debe tener el counter en 100', () => {
+        
+    const { result } = renderHook( () => useCounter(100) );
+
+    expect( result.current.counter ).toBe( 100 );
+
+
+   });
+
+   test('debe de incrementar el counter en 1', () => {
+        
+     const { result } = renderHook( () => useCounter( 100 ) );
+     const { increment } = result.current;
+
+     act(
+         () => {
+             increment();
+         }
+     );
+
+     const { counter } = result.current;
+     expect( counter ).toBe( 101 );
+
+
+   });
+   
+   test('debe de decrementar el counter en 1', () => {
+        
+    const { result } = renderHook( () => useCounter(100) );
+    const { decrement } = result.current;
+
+    act(
+        () => {
+            decrement();
+        }
+    );
+
+    const { counter } = result.current;
+    expect( counter ).toBe( 99 );
+
+   });
+   
+   test('debe de resetear el valor a su estado inicial', () => {
+
+        const { result } = renderHook( () => useCounter(100));
+
+        const { increment, reset } = result.current;
+
+        act(
+            () => {
+                increment();
+                reset();
+            }
+        );
+
+        const { counter } = result.current;
+
+        expect( counter ).toBe( 100 );
+
+   });  
+   
+
 });
